f92e1383eb86cb7befb26c072db6e6d3e0c70e87
kohei8281 <kohei8281@gmail.com>
2023-05-10 04:26:49 +0900

    Add file add button menu

--- app/FileViewController.swift
+++ app/FileViewController.swift
@@ -12,7 +12,17 @@ class FileViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     
-        let addItemButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(createFile(_:)))
+        let addItemButton: UIBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: nil)
+        
+        var addActions = [UIMenuElement]()
+        addActions.append(UIAction(title: "New Folder", image: UIImage(systemName: "folder.badge.plus") ,handler: { _ in
+            create_directory("/root/hellodir")
+        }))
+        addActions.append(UIAction(title: "New File", image: UIImage(systemName: "doc.badge.plus"), handler: { _ in
+            create_file("/root/hello")
+        }))
+
+        addItemButton.menu = UIMenu(title: "", options: .displayInline, children: addActions)
         self.navigationItem.rightBarButtonItem = addItemButton
     }
     
