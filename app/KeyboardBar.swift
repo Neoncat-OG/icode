4a906a41712c3d38307852336f94287b4de8ad26
kohei8281 <kohei8281@gmail.com>
2023-06-09 21:30:31 +0900

    Implement keyboard hide button

--- app/KeyboardBar.swift
+++ app/KeyboardBar.swift
@@ -11,28 +11,59 @@ import UIKit
 
 class KeyboardBar: UIInputView {
     
+    private var currentViewController: UIViewController? = nil
+    
     @IBOutlet weak var tabKey: UIButton!
+    @IBOutlet weak var hideButton: UIButton!
     
     @objc public func initBar() {
         print("Hello, KeyboardBar")
         self.tabKey.setTitle(nil, for: UIControl.State.normal)
         self.tabKey.setImage(UIImage(systemName: "arrow.right.to.line.alt"), for: UIControl.State.normal)
+        self.hideButton.setImage(UIImage(systemName: "keyboard.chevron.compact.down"), for: UIControl.State.normal)
         resize()
     }
     
-    @objc public func resize() {
+    func resize() {
         let bar = self.bounds.size
         
         if (UIDevice.current.userInterfaceIdiom == .phone) {
             // phone
-            self.tabKey.widthAnchor.constraint(equalToConstant: 32).isActive = true
-            
+            setButtonWidthSize(width: 32)
         } else if (bar.width >= 450) {
             // wide ipad
-            self.tabKey.widthAnchor.constraint(equalToConstant: 43).isActive = true
+            setButtonWidthSize(width: 43)
         } else {
             // narrow ipad (slide over)
-            self.tabKey.widthAnchor.constraint(equalToConstant: 36).isActive = true
+            setButtonWidthSize(width: 36)
+        }
+    }
+    
+    func setButtonWidthSize (width: CGFloat) {
+        self.tabKey.widthAnchor.constraint(equalToConstant: width).isActive = true
+        self.hideButton.widthAnchor.constraint(equalToConstant: width).isActive = true
+    }
+    
+    func topViewController(controller: UIViewController?) -> UIViewController? {
+        if let tabController = controller as? UITabBarController {
+            if let selected = tabController.selectedViewController {
+                return topViewController(controller: selected)
+            }
+        }
+        if let navigationController = controller as? UINavigationController {
+            return topViewController(controller: navigationController.visibleViewController)
+        }
+        if let presented = controller?.presentedViewController {
+            return topViewController(controller: presented)
+        }
+        return controller
+    }
+    
+    @IBAction func pressHideButton(_ sender: Any) {
+        if let parent = self.parentViewController() {
+            if let top = topViewController(controller: parent) {
+                top.view.endEditing(true)
+            }
         }
     }
 }
